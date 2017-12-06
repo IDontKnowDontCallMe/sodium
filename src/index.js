57588f9bf2dfbd9c50085cfaed530f37898c3968
151250126 <151250126@smail.nju.edu.cn>
2017-12-07 00:24:03 +0800

    ojbk

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
+import App from './app';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'semantic-ui-css/semantic.min.css';
-
-
-import {Provider} from 'react-redux';
-import store from './store'
-import {history} from './store'
-
-import { Route } from 'react-router-dom'
-import { ConnectedRouter} from 'react-router-redux'
-
-import MainContainer from './containers/MainContainer'
-
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <MainContainer/>
-        </ConnectedRouter>
-    </Provider>
+    <App/>
 
 , document.getElementById('app')
 );
